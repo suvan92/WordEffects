4b61ab0519511a0b7fe2f21c8d43c135a74bb635
Suvan <suvanramani@gmail.com>
2016-10-31 17:55:38 -0700

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Suvan Ramani on 2016-10-31.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
